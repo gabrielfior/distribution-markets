8bb2da9a5d62da30bef51e8ed579ba1ee3835e05
gabrielfior <fior.gbm@gmail.com>
2026-05-06 13:58:33 -0300

    fix: add Connect Wallet button to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth/RainbowKitCustomConnectButton";
+
 export const Header = () => {
-  return null;
+  return (
+    <div className="sticky top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-4">
+      <div className="navbar-start w-auto">
+        <Link href="/" className="flex items-center gap-2 ml-2">
+          <span className="font-bold text-lg">Distribution Markets</span>
+        </Link>
+      </div>
+      <div className="navbar-end flex-grow">
+        <RainbowKitCustomConnectButton />
+      </div>
+    </div>
+  );
 };
